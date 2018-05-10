272a81a438bec3364a1e5c3d113a1c7d8d3d6a82
love-somnus <928200207@qq.com>
2018-05-10 23:09:13 +0800

    @Value和随机值

--- SpringBoot-Maven-01-HelloWorld/src/main/java/com/somnus/springboot/controller/HelloWorldController.java
+++ SpringBoot-Maven-01-HelloWorld/src/main/java/com/somnus/springboot/controller/HelloWorldController.java
@@ -1,12 +1,12 @@
 package com.somnus.springboot.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-	@RequestMapping("/hello")
+	@GetMapping("/hello")
 	public String hello() {
 		return "Hello World";
 	}
