2016497dbc8f3ca4de05cd46dc63fe06b2e33b99
love-somnus <928200207@qq.com>
2018-08-14 18:43:14 +0800

    love-somnus <928200207@qq.com>

--- SpringBoot-Maven-14-Redis/src/test/java/com/somnus/springboot/RedissonLockTest.java
+++ SpringBoot-Maven-14-Redis/src/test/java/com/somnus/springboot/RedissonLockTest.java
@@ -21,28 +21,31 @@ public class RedissonLockTest {
 	@Autowired
 	private RedissonClient redisson ;
 	
-	private CountDownLatch cdl = new CountDownLatch(10);
+	private CountDownLatch cdl = new CountDownLatch(100);
 	
-	private int total = 100;
+	private int total = 10;
 	
+	/** 模拟100个人抢 10张票 ，没抢到就等待，直到拿到锁*/
 	@Test
 	public void lock() throws Exception {
 	    String key = UUID.randomUUID().toString();
-	    ExecutorService executor = Executors.newFixedThreadPool(10);
-        for (int i = 1; i <=10; i++){
+	    ExecutorService executor = Executors.newFixedThreadPool(100);
+        for (int i = 1; i <=100; i++){
             executor.execute(() -> {
-            	while(total>0) {
-            		RLock lock = redisson.getLock(key);
-                    lock.lock();
-                    try{
-            	        System.out.println(Thread.currentThread().getName() + " 剩余票数 " + (--total));
-            	        TimeUnit.MILLISECONDS.sleep(new Random().nextInt(100));// 为了测试出效果，让每次任务执行都需要一定时间
-            	    }catch(Exception e){
-            	    	e.printStackTrace();
-            	    }finally{
-            	        lock.unlock();
-            	    }
-            	}
+            	RLock lock = redisson.getLock(key);
+            	lock.lock();
+                try {
+                	if(total>0) {
+                		System.out.println(Thread.currentThread().getName() + " 抢到第（" + (total--) +"）张票 ");
+            	        TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1000));// 为了测试出效果，让每次任务执行都需要一定时间
+                	}else {
+                		System.out.println(Thread.currentThread().getName() + " 没有抢到票 ");
+                	}
+                }catch (Exception e) {
+                	e.printStackTrace();
+				}finally{
+        	        lock.unlock();
+        	    }
             	cdl.countDown();
             });
         }
@@ -53,28 +56,26 @@ public class RedissonLockTest {
 	@Test
 	public void tryLock() throws Exception {
 	    String key = UUID.randomUUID().toString();
-	    ExecutorService executor = Executors.newFixedThreadPool(10);
-        for (int i = 1; i <=10; i++){
+	    ExecutorService executor = Executors.newFixedThreadPool(100);
+        for (int i = 1; i <=100; i++){
             executor.execute(() -> {
-            	while(total>0) {
-            		RLock lock = redisson.getLock(key);
+            	RLock lock = redisson.getLock(key);
+            	//尝试加锁，支持过期解锁功能,10秒钟以后自动解锁, 无需调用unlock方法手动解锁
+                /*boolean isLock = lock.tryLock(10, TimeUnit.SECONDS);*/
+        		//尝试加锁，最多等待3秒，上锁以后10秒自动解锁
+                /*boolean isLock = lock.tryLock(3, 10, TimeUnit.SECONDS);*/
+    			boolean isLock = lock.tryLock();
+            	if(isLock) {
             		try {
-            			//尝试加锁，支持过期解锁功能,10秒钟以后自动解锁, 无需调用unlock方法手动解锁
-                        /*boolean isLock = lock.tryLock(10, TimeUnit.SECONDS);*/
-            			
-                		//尝试加锁，最多等待3秒，上锁以后10秒自动解锁
-                        /*boolean isLock = lock.tryLock(3, 10, TimeUnit.SECONDS);*/
-            			
-            			boolean isLock = lock.tryLock();
-                        if(isLock) {
-                        	System.out.println("isLock=" + isLock + "|" +Thread.currentThread().getName() + " 剩余票数 " + (--total));
-                	        TimeUnit.MILLISECONDS.sleep(new Random().nextInt(100));// 为了测试出效果，让每次任务执行都需要一定时间
-                        }
-					} catch (Exception e) {
-						e.printStackTrace();
-					} finally {
-						lock.unlock();
-					}
+            			System.out.println(Thread.currentThread().getName() + " 抢到第（" + (total--) +"）张票 ");
+            	        TimeUnit.MILLISECONDS.sleep(new Random().nextInt(1000));// 为了测试出效果，让每次任务执行都需要一定时间
+            		}catch (Exception e) {
+            			e.printStackTrace();
+					}finally{
+	        	        lock.unlock();
+	        	    }
+            	}else {
+            		System.out.println(Thread.currentThread().getName() + " 没有抢到票 ");
             	}
             	cdl.countDown();
             });
